ad28996aeeb038d05c7723f04ee5075c8a22c43f
artbast <eddperezg96@hotmail.com>
2018-05-05 22:57:11 -0600

    import XML libraries

---
+++ src/clases/XMLSalida.java
@@ -0,0 +1,33 @@
+package clases;
+
+
+/*
+ * Paquetes y librerias necesarias para generar el  documento XML
+ */
+
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.DOMImplementation;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Text;
+
+
+
+public class XMLSalida {
+    
+    
+    
+}
